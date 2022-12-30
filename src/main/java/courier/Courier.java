bb1e3d63d90819948cc8ffa065718f6f09228d0b
Elena Zhikhareva <elenazhikhareva08@yandex.ru>
2022-12-30 13:10:18 +0300

    внесены правки

---
+++ src/main/java/courier/Courier.java
@@ -0,0 +1,19 @@
+package courier;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class Courier {
+    private final String login;
+    private final String password;
+    private final String firstName;
+
+    public Courier(String login, String password, String firstName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+    }
+
+    public static Courier getRandomCourier() {
+        return new Courier(RandomStringUtils.randomAlphanumeric(10) + "", "pass", "12");
+    }
+}
